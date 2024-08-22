10aafc72ea0876223f3730572c2f223e3f57978f
Anargya Widyadhana <37019778+zephyrzth@users.noreply.github.com>
2024-08-22 19:43:12 +0800

    add login module

--- src/gotongroyong_frontend/src/App.jsx
+++ src/gotongroyong_frontend/src/App.jsx
@@ -1,31 +1,42 @@
-import { useState } from 'react';
-import { gotongroyong_backend } from 'declarations/gotongroyong_backend';
+import React from "react";
+import LoggedOut from "./LoggedOut";
+import { useAuth, AuthProvider } from "./use-auth-client";
+import "./assets/main.css";
+import LoggedIn from "./LoggedIn";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    gotongroyong_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
+  const { isAuthenticated, identity } = useAuth();
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    <>
+      <header id="header">
+        <section id="status" className="toast hidden">
+          <span id="content"></span>
+          <button className="close-button" type="button">
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </section>
+      </header>
+      <main id="pageContent">
+        {isAuthenticated ? <LoggedIn /> : <LoggedOut />}
+      </main>
+    </>
   );
 }
 
-export default App;
+export default () => (
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+);
